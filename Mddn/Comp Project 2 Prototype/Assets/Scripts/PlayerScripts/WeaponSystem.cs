52f9f80584eabeb337c6c62a5b690c4dfd2436a6
Nickel64 <snellgnich@ecs.vuw.ac.nz>
2018-08-07 10:28:45 +1200

    Initial set up of project, and first pass completion of 3rd person camera. Closes #2

---
+++ Mddn/Comp Project 2 Prototype/Assets/Scripts/PlayerScripts/WeaponSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour {
+	
+	public GameObject ship;
+	
+	//range of the weapon
+	//used externally for targeting reticule
+	public static float range = 200;
+	public int shotsToCooldown = 32;
+	public int currentShots = 0;
+	
+	void Start () {
+		//currentShots = 0;
+	}
+	
+	void Update () {
+		
+	}
+	
+	void onMouseDown() {
+		shoot();
+	}
+	
+	
+	///////////
+	//UTILITY//
+	///////////
+	
+	public bool isObjectInRange() {
+		//stub for hit/range detection - implement later
+		return true;
+	}
+	
+	
+	public bool shoot() {
+		//stub for shooting system - implement later
+		return true;
+	}
+
+	
+	///////////
+	//GETTERS//
+	///////////
+	
+	public float getRange() {
+		return range;
+	}
+}
