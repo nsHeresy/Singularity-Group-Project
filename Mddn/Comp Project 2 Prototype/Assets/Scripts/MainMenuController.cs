6835364124ec0b470c121e5612ad8c3c2a884c17
Nickel64 <snellgnich@ecs.vuw.ac.nz>
2018-08-15 09:31:23 +1200

    Refactored repo with a new gitignore, reorganising to reduce repo size, and some work on #4

---
+++ Mddn/Comp Project 2 Prototype/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+    
+    public void NewGame() {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void About() {
+        Debug.Log("About Button - Unimplemented");
+    }
+
+    public void Credits() {
+        Debug.Log("Credits Button - Unimplemented");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
---
+++ Mddn/Comp Project 2 Prototype/Assets/Scripts/Targetable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targetable : MonoBehaviour {
+
+
+    public GameObject entity;
+    public float health;
+    public bool isDead = false;
+
+    //Animations and Particles
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void damage(float amount) {
+        health -= amount;
+        if (health <= 0) {
+            StartCoroutine(Death());
+        }
+    }
+
+    IEnumerator Death() {
+
+        isDead = true;
+        playDeathAnimation();
+        GameObject.Destroy(entity);
+        yield return null;
+    }
+
+    void playDeathAnimation() {
+        Debug.Log("It's dead, Jim");
+        //Explosion, followed by phase out (Brackeys)
+    }
+}
