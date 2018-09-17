b9ebb9b3ef65e3a5907125fbb7d012cfff2cd7b0
Nicholas Snellgrove <snellgnich@ecs.vuw.ac.nz>
2018-09-18 11:27:02 +1200

    Some basic swarming behaviour set up as a starting point. Based on code from http://wiki.unity3d.com/index.php/Flocking. Related to issue #1

---
+++ Project 3/Assets/AI Testing/SwarmBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmBehaviour : MonoBehaviour {
+    private GameObject Controller;
+    private bool inited = false;
+    private float minVelocity;
+    private float maxVelocity;
+    private float randomness;
+    private GameObject chasee;
+    private Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        StartCoroutine("SwarmSteering");
+    }
+
+    IEnumerator SwarmSteering()
+    {
+        while (true)
+        {
+            if (inited)
+            {
+                body.velocity = body.velocity + Calc() * Time.deltaTime;
+
+                // enforce minimum and maximum speeds for the boids
+                float speed = body.velocity.magnitude;
+                if (speed > maxVelocity)
+                {
+                    body.velocity = body.velocity.normalized * maxVelocity;
+                }
+                else if (speed < minVelocity)
+                {
+                    body.velocity = body.velocity.normalized * minVelocity;
+                }
+            }
+
+            float waitTime = Random.Range(0.3f, 0.5f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private Vector3 Calc()
+    {
+        Vector3 randomize = new Vector3((Random.value * 2) - 1, (Random.value * 2) - 1, (Random.value * 2) - 1);
+
+        randomize.Normalize();
+        SwarmController swarmController = Controller.GetComponent<SwarmController>();
+        Vector3 flockCenter = swarmController.flockCenter;
+        Vector3 flockVelocity = swarmController.flockVelocity;
+        Vector3 follow = chasee.transform.localPosition;
+
+        flockCenter = flockCenter - transform.localPosition;
+        flockVelocity = flockVelocity - body.velocity;
+        follow = follow - transform.localPosition;
+
+        return (flockCenter + flockVelocity + follow * 2 + randomize * randomness);
+    }
+
+    public void SetController(GameObject theController)
+    {
+        Controller = theController;
+        SwarmController swarmController = Controller.GetComponent<SwarmController>();
+        minVelocity = swarmController.minVelocity;
+        maxVelocity = swarmController.maxVelocity;
+        randomness = swarmController.randomness;
+        chasee = swarmController.chasee;
+        inited = true;
+    }
+}
---
+++ Project 3/Assets/AI Testing/SwarmController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmController : MonoBehaviour {
+
+    public float minVelocity = 5;
+    public float maxVelocity = 20;
+    public float randomness = 1;
+    public int flockSize = 3;
+    public GameObject prefab;
+    public GameObject chasee;
+
+    public Vector3 flockCenter;
+    public Vector3 flockVelocity;
+
+    private GameObject[] entities;
+
+    void Start() {
+
+        Collider swarmBound = GetComponent<Collider>();
+
+        entities = new GameObject[flockSize];
+        for (var i = 0; i < flockSize; i++)
+        {
+            Vector3 position = new Vector3(
+                Random.value * swarmBound.bounds.size.x,
+                Random.value * swarmBound.bounds.size.y,
+                Random.value * swarmBound.bounds.size.z
+            ) - swarmBound.bounds.extents;
+
+            GameObject entity = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
+            entity.transform.parent = transform;
+            entity.transform.localPosition = position;
+            entity.GetComponent<SwarmBehaviour>().SetController(gameObject);
+            entities[i] = entity;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 theCenter = Vector3.zero;
+        Vector3 theVelocity = Vector3.zero;
+
+        foreach (GameObject entity in entities)
+        {
+            theCenter = theCenter + entity.transform.localPosition;
+            theVelocity = theVelocity + entity.GetComponent<Rigidbody>().velocity;
+        }
+
+        flockCenter = theCenter / (flockSize);
+        flockVelocity = theVelocity / (flockSize);
+    }
+}
