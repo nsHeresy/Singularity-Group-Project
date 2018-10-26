99ca17e26c67e99cbecfabee549337ce02ee620b
Nicholas Snellgrove <snellgnich@ecs.vuw.ac.nz>
2018-10-27 12:18:29 +1300

    Player Icon on Minimap now functioning correctly, keeps consistent rotation. Working on enemy icons on minimap. Related to issue #23

--- Project3/Assets/Scripts/Spaceflight Controls/PlayerFlightControl.cs
+++ Project3/Assets/Scripts/Spaceflight Controls/PlayerFlightControl.cs
@@ -199,6 +199,7 @@ public class PlayerFlightControl : MonoBehaviour
 	
 	}
 
+    //TESTING DRIVERS
     //-----------------------------------------------------------------------------------------------------
 
 
