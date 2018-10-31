bb972ffa7560eaa80ef5ced9a22e6c6c442bdab0
Nicholas Snellgrove <snellgnich@ecs.vuw.ac.nz>
2018-11-01 12:40:03 +1300

    Slight corrections throughout scene. Not strictly issue related.

---
+++ Project3/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public GameObject player;
+    public float healthPercent = 1f;
+   
+
+    private Player playerTargetable;
+    private float healthFullWidth;
+
+	// Use this for initialization
+	void Start () {
+        playerTargetable = player.GetComponent<Player>();
+        healthPercent = playerTargetable.CurHealth / playerTargetable.MaxHealth;
+        var healthBarRect = transform as RectTransform;
+        healthFullWidth = healthBarRect.sizeDelta.x;
+	}
+	
+	// After HP is updated
+	void LateUpdate ()
+    {
+        //reset the width of the health bar
+        var healthBarRect = transform as RectTransform;
+        healthBarRect.sizeDelta = new Vector2(healthFullWidth, healthBarRect.sizeDelta.y);
+
+        // calculate % width
+        healthPercent = playerTargetable.CurHealth / playerTargetable.MaxHealth;
+
+        //calculate new width and apply
+        var curWidth = healthBarRect.rect.width;
+        curWidth *= healthPercent;
+        healthBarRect.sizeDelta = new Vector2(curWidth, healthBarRect.sizeDelta.y);
+    }
+}
