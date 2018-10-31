db070ef4d648af72b7985032758b73e8feb6d8be
Nicholas Snellgrove <snellgnich@ecs.vuw.ac.nz>
2018-11-01 12:40:03 +1300

    Slight corrections throughout scene. Not strictly issue related.

---
+++ Project3/Assets/Scripts/MinimapComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapComponent : MonoBehaviour {
+
+    private Quaternion rot;
+    private float yPos;
+
+    private MinimapTracking minimap;
+    
+	void Start () {
+        rot = transform.rotation;
+        //minimap = GameObject.Find("MinimapCamera").GetComponent<MinimapTracking>();
+	}
+	
+	void LateUpdate () {
+
+        //transform.position = new Vector3(transform.position.x, minimap.transform.position.y + minimap.offset, transform.position.z);
+        transform.rotation = rot;
+	}
+}
