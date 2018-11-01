c9e6a20e16dfb8ea6a4f2b01080c9c8579e8cbd1
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-11-01 13:57:46 +1300

    Add score system to the player #31

--- Project3/Assets/Scripts/Player.cs
+++ Project3/Assets/Scripts/Player.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float CurHealth = 100;
     public float MaxHealth = 100;
-
-    public float HealFactor = 2;
-
+	public float HealFactor = 2;
+	
 	public static Vector3 PlayerPosition;
 
+	public static int CurrentScore;
+	
 	public Image HealthBar;
+	public Text ScoreText;
+	
 	
 	private void Update ()
 	{
 		PlayerPosition = transform.position;
 		HealthBar.fillAmount = CurHealth / MaxHealth;
+		ScoreText.text = CurrentScore.ToString();
         RegenHealth();
 	}
 
