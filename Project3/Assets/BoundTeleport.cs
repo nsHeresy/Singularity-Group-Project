06863cca8fa347ebe0a56aee727bed7e7067ead2
fellarg <fellarg117@gmail.com>
2018-11-01 13:20:12 +1300

    added effect to teleportation #12 #24

--- Project3/Assets/BoundTeleport.cs
+++ Project3/Assets/BoundTeleport.cs
@@ -2,22 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoundTeleport : MonoBehaviour {
 
     public GameObject player;           //the player object to move around
-
+    public Image panel;                 //the UI panel which can be darkened during this movement.
 
     //watches for when there is a collision
     private void OnTriggerExit(Collider other)
     {
 
+        
         Transform T = other.gameObject.GetComponent<Transform>();
 
         Vector3 pos = T.position;
         Vector3 newPos = pos * -1;
 
         player.transform.position = newPos;
+
+        //Color whiteFlash = new Color(255f, 255f, 255f, 255f);
+        // Color transparent = new Color(0f, 0f, 0f, 0f);
+        //panel.GetComponent<FadeIn>().PanelFade(whiteFlash, 1f, false);
+        //panel.GetComponent<FadeIn>().PanelFade(transparent, 20f, false);
+        panel.GetComponent<FadeIn>().Flash();
 
     }
 }
--- Project3/Assets/Scripts/FadeIn.cs
+++ Project3/Assets/Scripts/FadeIn.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class FadeIn : MonoBehaviour {
     
     public Image panel;
-    
+    Color whiteFlash = new Color(255f, 255f, 255f, 255f);
+    Color transparent = new Color(255f, 255f, 255f, 0f);
+
     /// <summary>
     /// Fades the given panel to a certain color & alpha, over time.
     /// </summary>
@@ -15,5 +17,9 @@ public class FadeIn : MonoBehaviour {
     /// <param name="ignoreTimeScale">Should this be a pauseable effect (basically)</param>
     public void PanelFade(Color color, float fadeTime, bool ignoreTimeScale) {
         panel.CrossFadeColor(color, fadeTime, ignoreTimeScale, true);
+    }
+    public void Flash(){
+        panel.color = whiteFlash;
+        panel.CrossFadeColor(transparent, 1f, false, true);
     }
 }
