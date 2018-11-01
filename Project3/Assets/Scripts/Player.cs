30ed91305c76163c9df97fc37a58865e3e618734
Nicholas Snellgrove <snellgnich@ecs.vuw.ac.nz>
2018-11-02 11:34:54 +1300

    Fixed the bug where the player is no longer able to die. Closes #28

--- Project3/Assets/Scripts/Player.cs
+++ Project3/Assets/Scripts/Player.cs
@@ -13,7 +13,11 @@ public class Player : MonoBehaviour
 	
 	public static Vector3 PlayerPosition;
 
-	public static int CurrentScore;
+    public ParticleSystem ExplodeParticle;
+    private bool Exploding = false;
+    public bool IsResponsive = true;
+
+    public static int CurrentScore;
 	
 	public Image HealthBar;
 	public Text ScoreText;
@@ -21,20 +25,26 @@ public class Player : MonoBehaviour
 	
 	private void Update ()
 	{
+
+        if (Exploding) return;
+
 		PlayerPosition = transform.position;
 		HealthBar.fillAmount = CurHealth / MaxHealth;
 		ScoreText.text = CurrentScore.ToString();
 
         //kill the player
         if (CurHealth <= 0) {
-            Kill();
+            StartCoroutine("Kill");
+            Exploding = true;
         }
         RegenHealth();
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
+        
         var magnitude = GetComponent<Rigidbody>().velocity.magnitude;
+        Debug.Log(collision.gameObject.name);
         Damage(magnitude * 5);
     }
 
@@ -48,8 +58,37 @@ public class Player : MonoBehaviour
             CurHealth = MaxHealth;
     }
 
-    void Kill() {
-        Debug.Log("Kill Me");
+    IEnumerator Kill() {
+
+        //trigger an explosion
+        ParticleSystem explosion = Instantiate(ExplodeParticle, transform.position, Quaternion.identity);
+        explosion.transform.parent = transform;
+
+        IsResponsive = false;
+
+        float totalDuration = explosion.main.duration;
+        yield return new WaitForSeconds(totalDuration);
+
+        //destroy the ship
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+
+        //trigger an explosion
+        explosion = Instantiate(ExplodeParticle, transform.position, Quaternion.identity);
+        explosion.transform.parent = transform;
+
+        totalDuration = explosion.main.duration;
+        yield return new WaitForSeconds(totalDuration);
+
+
+        explosion.Stop();
+        DestroyImmediate(explosion);
+        explosion = null;
+
+        Destroy(gameObject);
     }
 	
 }
