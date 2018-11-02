243c62c196141fc5ed3cee5d804aac7e25c289a1
fellarg <fellarg117@gmail.com>
2018-11-02 22:05:13 +1300

    Added AI shooting at player #25

---
+++ Project3/Assets/Scripts/AiShoot.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiShoot : MonoBehaviour {
+    private GameObject player;
+    public float range;
+    public float accuracy;
+
+    
+    private LineRenderer _gunLine;
+
+    //utils
+    private float _timer;
+    private float _timeBetweenShots = 1f;
+    private float _effectDisplayTime = 0.1f;
+
+    private void Start()
+    {
+       player = GameObject.Find("Characters/Player");
+    }
+
+    private void Awake()
+    {
+        _gunLine = GetComponent<LineRenderer>();
+    }
+
+
+        // Update is called once per frame
+        void LateUpdate () {
+        Vector3 target = Player.PlayerPosition;
+        float distance = Vector3.Distance(target, transform.position);
+        if(distance < range && _timer< _timeBetweenShots)
+        {
+            Shoot(target);
+        }
+        if (_timer >= _effectDisplayTime)
+        {
+            DisableEffects();
+        }
+    }
+
+    private void Shoot(Vector3 target)
+    {
+        _timer = 0f;
+        Vector3 direction = target - transform.position;
+        direction.x += UnityEngine.Random.Range(accuracy, -accuracy);
+        direction.y += UnityEngine.Random.Range(accuracy, -accuracy);
+        direction.z += UnityEngine.Random.Range(accuracy, -accuracy);
+        _gunLine.SetPosition(0, transform.position);
+       // _gunLine.SetPosition(1, target);
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, direction, out hit, range + 50))
+        {
+            Debug.DrawRay(transform.position, direction, Color.red, 0.3f);
+            Debug.Log("hit");
+
+            _gunLine.SetPosition(1, direction*range);
+            player.GetComponent<Player>().Damage(5);
+
+        }
+        else
+        {
+            _gunLine.SetPosition(1, direction * range);
+            Debug.DrawRay(transform.position, direction, Color.red, 0.3f);
+            Debug.Log("miss");
+        }
+    }
+    public void DisableEffects()
+    {
+        _gunLine.enabled = false;
+    }
+}
